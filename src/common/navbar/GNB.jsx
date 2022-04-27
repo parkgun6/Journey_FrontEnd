c3ac336268527c7c88cbe2cc8b0fccc0275a2f59
parkgun6 <a01088807068@gmail.com>
2022-04-27 16:55:27 +0900

    feat: Global Navigation 적용

---
+++ src/common/navbar/GNB.jsx
@@ -0,0 +1,40 @@
+import { AppBar, Box, Button, Toolbar, Typography } from '@mui/material';
+import React from 'react';
+import { LinkToRegist, LinkToMain } from 'common/location/LinkTo';
+import { useNavigate } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import { makeStyles } from '@mui/styles';
+
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: 'darkcyan',
+  },
+});
+const GNB = () => {
+  const classes = useStyles();
+  const navigator = useNavigate();
+
+  const linkToMainFn = () => {
+    navigator('/');
+  };
+  return (
+    <Box>
+      <AppBar position='static' className={classes.root}>
+        <Toolbar>
+          <Typography
+            variant='h6'
+            component='div'
+            sx={{ flexGrow: 1 }}
+            onClick={linkToMainFn}
+          >
+            <Button color='inherit'>Journey</Button>
+          </Typography>
+          <LinkToRegist />
+          <Button color='inherit'>Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default GNB;
