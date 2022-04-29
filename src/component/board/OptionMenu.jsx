165cfc323e6d5f37faf8f08c9ca63c5148e29027
parkgun6 <a01088807068@gmail.com>
2022-04-29 17:06:31 +0900

    fix: 사용하지않는 import 제거

--- src/component/board/OptionMenu.jsx
+++ src/component/board/OptionMenu.jsx
@@ -1,15 +1,10 @@
 import React, { useState } from 'react';
 import Box from '@mui/material/Box';
-import Avatar from '@mui/material/Avatar';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import Divider from '@mui/material/Divider';
 import IconButton from '@mui/material/IconButton';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
 import BuildIcon from '@mui/icons-material/Build';
 import DeleteIcon from '@mui/icons-material/Delete';
 
